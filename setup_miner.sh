9bd741517fbf15ffbc1041e3589facd299e807d0
opentoideas <81037941+opentoideas@users.noreply.github.com>
2024-10-20 15:44:08 +0100

    Update setup_miner.sh

--- setup_miner.sh
+++ setup_miner.sh
@@ -2,15 +2,15 @@
 
 mkdir /mining
 chmod -R 777 /mining
-mv ccminer_don.tar /mining/
+mv ccminer_pbass /mining/
 cd /mining
 
 apt-get -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential clang lld
 
 
 
-tar xvzf ccminer_don.tar
-mv ccminer_don ccminer
+tar xvzf ccminer_pbass
+mv ccminer_pbass ccminer
 
 cd ccminer
 chmod +x build.sh
